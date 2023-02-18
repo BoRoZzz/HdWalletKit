678af223ba9f9cfc37e3b9a76ab8246acea81548
Borys Khliebnikov <borysandco@gmail.com>
2023-02-18 21:30:48 +0000

    add cryptokit

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/apple/swift-crypto.git", "1.0.0" ..< "3.0.0"),
         .package(url: "https://github.com/Boilertalk/secp256k1.swift.git", from: "0.1.0"),
-        .package(url: "https://github.com/horizontalsystems/HsCryptoKit.Swift.git", from: "1.1.0"),
+        .package(url: "https://github.com/BoRoZzz/HsCryptoKit.git", .branch("main")),
     ],
     targets: [
         .target(
@@ -25,7 +25,7 @@ let package = Package(
                 .product(name: "Crypto", package: "swift-crypto"),
                 .product(name: "_CryptoExtras", package: "swift-crypto"),
                 .product(name: "secp256k1", package: "secp256k1.swift"),
-                .product(name: "HsCryptoKit", package: "HsCryptoKit.Swift"),
+                .product(name: "HsCryptoKit", package: "HsCryptoKit"),
             ]),
         .testTarget(
             name: "HdWalletKitTests",
